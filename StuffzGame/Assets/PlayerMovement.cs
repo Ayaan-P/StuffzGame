64e074a2fdf3a4854229787840040623b307317d
Anand Desai <desaiaj@rose-hulman.edu>
2020-05-04 00:28:40 +0530

    Added barebones movement script

---
+++ StuffzGame/Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rigidBody;
+    Vector2 movementVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame (don't put anything physics related here)
+    void Update()
+    {
+        // Input
+
+        movementVector.x = Input.GetAxisRaw("Horizontal");
+        movementVector.y = Input.GetAxisRaw("Vertical");
+
+
+    }
+
+    // Like Update() but executed on fixed timer (independent of framerate)
+    void FixedUpdate()
+    {
+        // Movement
+        rigidBody.MovePosition(rigidBody.position + movementVector * moveSpeed * Time.fixedDeltaTime);
+
+    }
+}
