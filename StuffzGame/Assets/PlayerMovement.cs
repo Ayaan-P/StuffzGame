025a96975c5e0ce6d6ccb6c6cebf6f1bff3adb56
apupala2 <wdd>
2020-05-04 01:04:55 -0500

    2D movement done, only keeps original input

--- StuffzGame/Assets/PlayerMovement.cs
+++ StuffzGame/Assets/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float moveSpeed = 5f;
-
+    public int current_axis=0;
     public Rigidbody2D rigidBody;
     Vector2 movementVector;
     // Start is called before the first frame update
@@ -18,13 +18,39 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame (don't put anything physics related here)
     void Update()
-    {
+    { 
         // Input
-
-        movementVector.x = Input.GetAxisRaw("Horizontal");
-        movementVector.y = Input.GetAxisRaw("Vertical");
-
-
+        if(current_axis==1)
+        { 
+            if(movementVector.x!=Input.GetAxisRaw("Horizontal"))
+            {
+                movementVector.y = 0;
+                movementVector.x = Input.GetAxisRaw("Horizontal");
+                current_axis = 0;
+            }
+            if(movementVector.y != Input.GetAxisRaw("Vertical"))
+            {
+                movementVector.x = 0;
+                movementVector.y = Input.GetAxisRaw("Vertical");
+                current_axis = 1;
+            }
+        }
+        if(current_axis==0)
+        {
+             if(movementVector.y != Input.GetAxisRaw("Vertical"))
+            {
+                movementVector.x = 0;
+                movementVector.y = Input.GetAxisRaw("Vertical");
+                current_axis = 1;
+            }
+            if(movementVector.x!=Input.GetAxisRaw("Horizontal"))
+            {
+                movementVector.y = 0;
+                movementVector.x = Input.GetAxisRaw("Horizontal");
+                current_axis = 0;
+            }
+        }
+    
     }
 
     // Like Update() but executed on fixed timer (independent of framerate)
