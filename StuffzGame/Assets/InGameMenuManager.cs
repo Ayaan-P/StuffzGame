c6e3e57da9dd04c0905289d76579585d6c438a8b
Anand Desai <desaiaj@rose-hulman.edu>
2020-05-11 02:18:54 +0530

    added in game menu that pauses game. Not functional at the moment

---
+++ StuffzGame/Assets/InGameMenuManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenuManager : MonoBehaviour
+{
+    public GameObject menu;
+
+    void Start()
+    {
+        // start without menu active
+        menu.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Boolean menuToggle = !menu.gameObject.activeSelf;
+            Debug.Log("Escape key was pressed. Menu toggled: " + menuToggle);
+
+            int timescale = menuToggle? 0 : 1;
+
+            PauseOrContinue(menuToggle, timescale);
+        }
+    }
+
+    void PauseOrContinue(Boolean toggle, int timescale)
+    {
+        Time.timeScale = timescale;
+        menu.gameObject.SetActive(toggle);
+
+        //Disable movement since it's NOT dependent on timescale
+        GameObject player = GameObject.Find("Player");
+        (player.GetComponent("PlayerMovement") as MonoBehaviour).enabled = !toggle;
+    }
+
+}
--- StuffzGame/Assets/PlayerMovement.cs
+++ StuffzGame/Assets/PlayerMovement.cs
@@ -7,9 +7,10 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float moveSpeed = 5f;
-    public int current_axis=0;
+    public int current_axis = 0;
     public Rigidbody2D rigidBody;
     Vector2 movementVector;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,39 +19,39 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame (don't put anything physics related here)
     void Update()
-    { 
+    {
         // Input
-        if(current_axis==1)
-        { 
-            if(movementVector.x!=Input.GetAxisRaw("Horizontal"))
-            {
-                movementVector.y = 0;
-                movementVector.x = Input.GetAxisRaw("Horizontal");
-                current_axis = 0;
-            }
-            if(movementVector.y != Input.GetAxisRaw("Vertical"))
-            {
-                movementVector.x = 0;
-                movementVector.y = Input.GetAxisRaw("Vertical");
-                current_axis = 1;
-            }
-        }
-        if(current_axis==0)
+        if (current_axis == 1)
         {
-             if(movementVector.y != Input.GetAxisRaw("Vertical"))
+            if (movementVector.x != Input.GetAxisRaw("Horizontal"))
+            {
+                movementVector.y = 0;
+                movementVector.x = Input.GetAxisRaw("Horizontal");
+                current_axis = 0;
+            }
+            if (movementVector.y != Input.GetAxisRaw("Vertical"))
             {
                 movementVector.x = 0;
                 movementVector.y = Input.GetAxisRaw("Vertical");
                 current_axis = 1;
             }
-            if(movementVector.x!=Input.GetAxisRaw("Horizontal"))
+        }
+        if (current_axis == 0)
+        {
+            if (movementVector.y != Input.GetAxisRaw("Vertical"))
+            {
+                movementVector.x = 0;
+                movementVector.y = Input.GetAxisRaw("Vertical");
+                current_axis = 1;
+            }
+            if (movementVector.x != Input.GetAxisRaw("Horizontal"))
             {
                 movementVector.y = 0;
                 movementVector.x = Input.GetAxisRaw("Horizontal");
                 current_axis = 0;
             }
         }
-    
+
     }
 
     // Like Update() but executed on fixed timer (independent of framerate)
