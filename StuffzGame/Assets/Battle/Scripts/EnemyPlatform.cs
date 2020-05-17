d31abb96bf04ce6756cef4099cea76d9db3b39f2
Anand Desai <desaiaj@rose-hulman.edu>
2020-05-18 00:16:46 +0530

    fundamentally restructured folder system. New format is now self composing i.e. Pokemon > Scene/ ; Scripts/ ; Sprites/ and so on for each folder group. Additionally fixed EnemyPlatform.cs and PlayerPlatform.cs to have animation code in FixedUpdate() instead of Update()

---
+++ StuffzGame/Assets/Battle/Scripts/EnemyPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPlatform : MonoBehaviour
+{
+    public Transform pos1,pos2;
+    public float speed;
+    public Transform startpos;
+
+    public Vector3 nextPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPos = startpos.position;
+    }
+
+    // FixedUpdate() is called on a fixed timer
+    void FixedUpdate()
+    {
+        nextPos= pos2.position;
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed*Time.deltaTime);
+    }
+}
