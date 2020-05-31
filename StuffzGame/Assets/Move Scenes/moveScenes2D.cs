072afde3c72a21a0a68beab22889aef23347ce99
Ayaan Khan <akhan117@terpmail.umd.edu>
2020-05-31 15:33:05 -0400

    lighting, sprites, tilemap

---
+++ StuffzGame/Assets/Move Scenes/moveScenes2D.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class moveScenes2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+	[SerializeField] private string insidePlace;
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("GameObject1 collided with " + other.name);
+		if(other.CompareTag("Player"))
+		{
+			SceneManager.LoadScene(insidePlace);
+		}
+	}
+}
---
+++ StuffzGame/Assets/moveScene2d2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class moveScene2d2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+	[SerializeField] private string outsidePlace;
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("GameObject1 collided with " + other.name);
+		if(other.CompareTag("Player"))
+		{
+			SceneManager.LoadScene(outsidePlace);
+		}
+	}
+}
