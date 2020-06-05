fba18a07720c5766a86b471632811aee44af9be4
apupala2 <wdd>
2020-06-05 17:47:23 -0500

    Overworld item prefab made

---
+++ StuffzGame/Assets/Interactable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 2f;
+    public GameObject player;
+    void Update()
+    {
+        if(Input.GetAxisRaw("Submit")>=0)
+        {
+            
+           
+            var heading = transform.position - player.GetComponent<Transform>().position;
+            var distance = heading.magnitude;
+            var direction = heading / distance;
+            var lookingin = player.GetComponent<PlayerMovement>().lookingin;
+
+            if(distance<=radius && Input.GetAxisRaw("Submit")>0)
+            {
+              
+                if( (Mathf.Round(direction.x) == lookingin.x) || (Mathf.Round(direction.y) == lookingin.y))
+                {
+                    
+                        Debug.Log(" You found a Blazikenite");
+                        Destroy(gameObject);
+                 
+                }
+                
+            }
+     
+        }
+    }
+}
