71b06ff0be82ee30cafc1a441dddbf47a7ac75f3
Anand Desai <desaiaj@rose-hulman.edu>
2020-06-15 18:49:49 +0530

    In game menu can switch between tabs. Added pokemon type,gender,move dmg class sprites

---
+++ StuffzGame/Assets/UI/InGameMenu/Scripts/MenuButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuButton : MonoBehaviour,IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+
+    public MenuGroup menuGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuGroup.SubscribeButton(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        menuGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        menuGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        menuGroup.OnTabExit(this);
+    }
+}
---
+++ StuffzGame/Assets/UI/InGameMenu/Scripts/MenuGroup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuGroup : MonoBehaviour
+{
+    private List<MenuButton> menuButtons;
+    public List<GameObject> menuPages;
+
+    public void SubscribeButton(MenuButton button)
+    {
+        if (menuButtons == null)
+        {
+            menuButtons = new List<MenuButton>();
+        }
+        menuButtons.Add(button);
+    }
+
+    public void OnTabEnter(MenuButton button) { }
+    public void OnTabSelected(MenuButton button) {
+        int buttonIndex = button.transform.GetSiblingIndex();
+
+        for(int pageIndex = 0; pageIndex < menuPages.Count; pageIndex++)
+        {
+            if (pageIndex == buttonIndex)
+            {
+                menuPages[pageIndex].SetActive(true);
+            }
+            else
+            {
+                menuPages[pageIndex].SetActive(false);
+
+            }
+        }
+    }
+    public void OnTabExit(MenuButton button) { }
+
+
+}
